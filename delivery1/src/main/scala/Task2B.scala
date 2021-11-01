56cd821fcdb01d4d4325f73cdb9fe8b98ca6b20a
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2021-11-01 11:53:16 +0100

    Completed Task2B

--- delivery1/src/main/scala/Task2B.scala
+++ delivery1/src/main/scala/Task2B.scala
@@ -1,3 +1,26 @@
 object Task2B extends App {
+  private var counter: Int = 0
+
+  for(i <- 1 to 2) {
+    new Thread() {
+      override def run() : Unit = {
+        increaseCounter();
+      }
+    }.start();
+  }
+
+  new Thread() {
+    override def run() : Unit = {
+      printCounter();
+    }
+  }.start();
+
+  def increaseCounter(): Unit = {
+    counter += 1
+  }
+
+  def printCounter(): Unit = {
+    println("Current counter: " + counter);
+  }
 
 }
