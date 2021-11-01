0029d27edbc1b1902c64b5fdd3e003d0f6e0fd82
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2021-11-01 11:18:51 +0100

    Completed task 1B and 1D

--- delivery1/src/main/scala/Task1D.scala
+++ delivery1/src/main/scala/Task1D.scala
@@ -1,3 +1,17 @@
 object Task1D extends App {
 
+  //Testing code
+  println(fib(15));
+
+  //Create a function to compute the nth Fibonacci number using recursion without using memoization
+  //(or other optimizations). Use BigInt instead of Int. What is the difference between these two data
+  //types?
+  def fib(n : BigInt) : BigInt = {
+    if(n == 1 || n == 2) {
+      return 1;
+    }
+    else {
+      return fib(n - 1) + fib(n - 2);
+    }
+  }
 }
