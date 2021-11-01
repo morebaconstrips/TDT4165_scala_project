f554177132b84fc4090b1db81b3a959e83e85489
Gabriele Abbate <gabrieab@stud.ntnu.no>
2021-11-01 11:54:16 +0100

    Completed task1C

--- delivery1/src/main/scala/Task1C.scala
+++ delivery1/src/main/scala/Task1C.scala
@@ -1,3 +1,15 @@
 object Task1C extends App {
 
+    var arr = Array(1, 2, 3, 4, 5)
+    var l = arr.length
+
+    def sum_recursively(array : Array[Int], len : Int): Int = {
+        if (len == 1) {
+            return array(0)
+        }
+        else {
+            return sum_recursively(array, len-1) + array(len-1)
+        }    
+    }
+    println(sum_recursively(arr, l))
 }
