db9e1731c3bdbea09b36a9a278fb1197f5aeb7b4
BlackKnight625 <43484611+BlackKnight625@users.noreply.github.com>
2021-11-01 17:44:10 +0100

    1.2 1.3 Implemented funcitons on Account.scala

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -1,17 +1,71 @@
 import exceptions._
 
+import java.util.concurrent.locks.{ReadWriteLock, ReentrantReadWriteLock}
+
 class Account(val bank: Bank, initialBalance: Double) {
 
-    class Balance(var amount: Double) {}
+    class Balance(var amount: Double) {
+        val rwLock : ReadWriteLock = new ReentrantReadWriteLock();
+
+        def readLock(): Unit = rwLock.readLock().lock();
+        def readUnlock(): Unit = rwLock.readLock().unlock();
+        def writeLock(): Unit = rwLock.writeLock().lock();
+        def writeUnlock(): Unit = rwLock.writeLock().unlock();
+    }
 
     val balance = new Balance(initialBalance)
 
     // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
+    def withdraw(amount: Double): Either[Unit, String] = {
+        var either : Either[Unit, String] = null;
+
+        balance.writeLock();
+
+        if(balance.amount > amount) {
+            if(amount >= 0) {
+                balance.amount -= amount;
+                either = Left();
+            }
+            else {
+                either = Right("The amount to withdraw must be greater than 0");
+            }
+        }
+        else {
+            either = Right("There's not enough balance for the withdrawal");
+        }
+
+        balance.writeUnlock();
+
+        return either;
+    }
+    def deposit (amount: Double): Either[Unit, String] = {
+        var either : Either[Unit, String] = null;
+
+        balance.writeLock();
+
+        if(amount >= 0) {
+            balance.amount += amount;
+            either = Left();
+        }
+        else {
+            either = Right("The amount to deposit must be greater than 0");
+        }
+
+        balance.writeUnlock();
+
+        return either;
+    }
+    def getBalanceAmount: Double       = {
+        balance.readLock();
+
+        val amount = balance.amount;
+
+        balance.readUnlock();
+
+        return amount;
+    }
 
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
